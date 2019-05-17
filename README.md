779abd4113edab23f50cac8172a8c2b4019a4b53
Motohiko Makino <motohiko.makino@gmail.com>
2019-05-17 09:53:19 -0700

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# Motohiko Makino
-
-### Software Engineer
-
-### Bio
-* https://mmakino.github.io/index.html
-### Portfolio
-* https://mmakino.github.io/portfolio.html
---
+++ precache-manifest.f62f327babdb869d43f26d82fdddf675.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07f503f2e14bcfe612c66916fce73d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0473c49d88a047530b6",
+    "url": "/static/css/main.82db85f5.chunk.css"
+  },
+  {
+    "revision": "8f3f458857a8853bd52b",
+    "url": "/static/js/2.868329b5.chunk.js"
+  },
+  {
+    "revision": "e0473c49d88a047530b6",
+    "url": "/static/js/main.bc75b21c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
