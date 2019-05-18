5cf4db7fc834689ac7a18f61e86af0a40ac1e52d
Motohiko Makino <motohiko.makino@gmail.com>
2019-05-17 20:44:49 -0700

    Updates

---
+++ precache-manifest.7fa0df899c97bc45ca1a71e676eecdd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31658e031c8ab8f9bd317706a765ff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e95293ad10506cb436d8",
+    "url": "/static/css/main.f3c6824a.chunk.css"
+  },
+  {
+    "revision": "2ebada7736fa2a9a9559",
+    "url": "/static/js/2.75d01af1.chunk.js"
+  },
+  {
+    "revision": "e95293ad10506cb436d8",
+    "url": "/static/js/main.a20c7d4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43761012bf1ec698b497ad73ba3c545d.js"
+  "/precache-manifest.7fa0df899c97bc45ca1a71e676eecdd5.js"
 );
 
 self.addEventListener('message', (event) => {
